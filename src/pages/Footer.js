6a6f84abfc05938e1997de6e9e4e2b20c5e88f7a
N4thanJ <nathan.jordens@student.ucll.be>
2024-03-06 17:35:45 +0100

    updated footer

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -6,14 +6,14 @@ function Footer() {
             <article className="flex flex-col items-center gap-5 py-10 bg-secondary">
                 <div className="text-white text-center">
                     <h2 className="text-2xl">Nathan Jordens</h2>
-                    <p className="text-grey px-5">Join me on my journey on becomming a dev!</p>
+                    <p className="text-grey px-5">Join me on my journey on becomming a junior dev!</p>
                 </div>
                 <SocialButtons />
             </article>
 
             <article className="bg-seablue text-white p-4 text-sm flex md:flex-row flex-col justify-between">
-                <p>Copyright &copy;2024 <a className="text-tertiair underline" href="https://github.com/N4thanJ" target="_blank">N4thanJ</a></p>
-                <nav>
+                <p>Copyright &copy;2024 <a className="text-tertiair underline text-center" href="https://github.com/N4thanJ" target="_blank">N4thanJ</a></p>
+                <nav className="md:block hidden">
                     <ul className="flex gap-2">
                         <li><a href="#">Home</a></li>
                         <li><a href="#">About</a></li>
